c0401898f702d75d224b3b970720b37e88b87198
Andras Varga <zoryandras@gmail.com>
2022-05-19 15:09:09 +0200

    Made pages with gatsby!

---
+++ gatsby-starter-hello-world/src/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>hello from about page</div>
+  )
+}
+
+export default about
---
+++ gatsby-starter-hello-world/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import {Link} from 'gatsby'
+
+export default function Home() {
+  return <div>
+    <h1>Yay!</h1>
+    <div>
+      <Link to="/about">about</Link>
+    </div>
+  </div>
+}
