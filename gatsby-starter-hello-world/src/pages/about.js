7f3188d481dc012a63ea19cd6a471ef4d83d93fe
Andras Varga <zoryandras@gmail.com>
2022-05-20 12:26:37 +0200

    Made Footer, Navbar and Layout components!

--- gatsby-starter-hello-world/src/pages/about.js
+++ gatsby-starter-hello-world/src/pages/about.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Layout from '../components/Layout'
+
 
 function about() {
   return (
-    <div>hello from about page</div>
+    <Layout>
+      <div>hello from about page</div>
+    </Layout>
   )
 }
 
--- gatsby-starter-hello-world/src/pages/index.js
+++ gatsby-starter-hello-world/src/pages/index.js
@@ -1,16 +1,18 @@
 import React from "react"
 import {Link} from 'gatsby'
+import Layout from "../components/Layout"
+
 
 export default function Home() {
   return (
-    <div>
-      <h1>Yay!</h1>
+    <Layout>
+      <h1>HOME PAGE</h1>
       <div>
         <Link to="/about">about</Link>
       </div>
       <div>
         <Link to="/company/history">history</Link>
       </div>
-    </div>
+    </Layout>
   )
 }
