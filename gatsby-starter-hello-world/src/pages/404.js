ed279273f4c39bab5b680e98bc67834d24b79e29
Andras Varga <zoryandras@gmail.com>
2022-06-18 10:49:36 +0200

    Modified 404.js

--- gatsby-starter-hello-world/src/pages/404.js
+++ gatsby-starter-hello-world/src/pages/404.js
@@ -8,7 +8,7 @@ const Error = () => {
       <SEO title="Error" />
       <main className="error-page">
         <section>
-          <h1>404</h1>
+          <h1>so this is the 404 page</h1>
           <h3>oh gosh! there's nothing here</h3>
         </section>
       </main>
