d867763dc26939dd0298d494262a86562e3cba07
Andras Varga <zoryandras@gmail.com>
2022-06-18 10:45:08 +0200

    Changed 404.js message

--- gatsby-starter-hello-world/src/pages/404.js
+++ gatsby-starter-hello-world/src/pages/404.js
@@ -9,7 +9,7 @@ const Error = () => {
       <main className="error-page">
         <section>
           <h1>404</h1>
-          <h3>oh gosh, there is nothing here!</h3>
+          <h3>oh gosh! there's nothing here</h3>
         </section>
       </main>
     </Layout>
